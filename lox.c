c9861f8f9d946830933a42e71a5670608e94038a
Jim-Cottet <jimcottet.pro@gmail.com>
2024-09-30 19:23:13 +0200

    Fixes #1, #2: transfert the reading file loop into scanner/handle start count/create global variables for the line_number

--- lox.c
+++ lox.c
@@ -1,48 +1,31 @@
-//* Std
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-
-//* Perso
-#include "scanner.h"
+#include "scanner.c"
 
 //* Function declaration
 void run_prompt();
 void run_file();
 
-int main(int argc, char *argv[]) {
-
+int main(int argc, char *argv[]) 
+{
     if (argc > 2) 
     {
         printf("Usage %s <filename>\n", argv[0]);
         return 1;
     }
-
     if (argc == 2)
         run_file(argv[1]);
     else
         run_prompt();
-
     return 0;
 }
 
 void run_file(char *filename)
 {
-    
     FILE *file = fopen(filename, "r");
-    //! Loop to put in the scanner.h
-    char line[256];
-    int line_number = 0;
-
-    while (fgets(line, sizeof(line), file))
-    {
-        line_number++;
-        printf("Results for the line #%d \n", line_number);
-        scan_tokens(line);
-    }
-    
-    fclose(file);
+    scan_file(file);
 }
 
 void run_prompt()
@@ -52,13 +35,10 @@ void run_prompt()
     for(;;)
     {
         printf("> ");
-
         if (fgets(line, sizeof(line), stdin) == NULL)
             break;
-
         line[strcspn(line, "\n")] = '\0';
-
-        run(line, 1);
+        //Need something to run here but what?
     }
 }
 
---
+++ scanner.c
@@ -0,0 +1,126 @@
+#include "scanner.h"
+
+int line_number = 0;
+
+void scan_file(FILE *file)
+{
+    char line[256];
+
+    line_number = 0; // make the global variable 0 before the beginning of the loop
+
+    while (fgets(line, sizeof(line), file))
+    {
+        printf("Results for the line #%d \n", line_number);
+        scan_tokens(line);
+        line_number++;
+    }
+    
+    fclose(file);
+}
+
+void scan_tokens(char *source)
+{
+    int token_count = 0;
+    int token_capacity = 8;
+    int start = 0;
+    int current = 0;
+
+    Token **tokens = (Token**)malloc(token_capacity * sizeof(Token*));
+    if (tokens == NULL) {
+        perror("Failed to allocate memory");
+        exit(EXIT_FAILURE);
+    }
+    
+    while (current <= strlen(source))
+    {
+        if (token_count >= token_capacity) {
+            token_capacity *= 2;
+            tokens = (Token**)realloc(tokens, token_capacity * sizeof(Token*));
+            if (tokens == NULL) {
+                perror("Failed to reallocate memory");
+                exit(EXIT_FAILURE);
+            }
+        }
+
+        char c = source[current];
+        if (isspace(c))
+        {
+            current++;
+            start = current;
+            continue;
+        }
+        if (c != '\0') 
+        {
+            tokens[token_count] = (Token*)malloc(sizeof(Token)); // Allocate memory for each token
+            if (tokens[token_count] == NULL) {
+                perror("Failed to allocate memory for token");
+                exit(EXIT_FAILURE);
+            }
+            Token temp = scan_token(c);
+            if (temp.type != 38) {
+                *tokens[token_count] = temp;
+                printf("Type: %d\n", temp.type);
+                token_count++;
+            }
+        }
+        current++;
+    }
+
+    // Free allocated memory
+    for (int i = 0; i < token_count; i++) {
+        free(tokens[i]);
+    }
+    free(tokens);
+}
+
+Token scan_token(char c)
+{
+    Token result;
+    switch (c)
+    {
+    case '(':
+        return add_token(LEFT_PAREN);
+        break;
+    case ')':
+        return add_token(RIGHT_PAREN);
+        break;
+    case '{':
+        return add_token(LEFT_BRACE);
+        break;
+    case '}':
+        return add_token(RIGHT_BRACE);
+        break;
+    case ',':
+        return add_token(COMMA);
+        break;
+    case '.':
+        return add_token(DOT);
+        break;
+    case '-':
+        return add_token(MINUS);
+        break;
+    case '+':
+        return add_token(PLUS);
+        break;
+    case ';':
+        return add_token(SEMICOLON);
+        break;
+    case '*':
+        return add_token(STAR);
+        break;
+    default:
+        fprintf(stderr, "Unexpected character '%c'\n", c);
+    }
+    return add_token(ERROR);
+}
+
+Token add_token(TokenType type)
+{
+    Token token;
+    token.type = type;
+    token.lexeme = NULL;
+    token.literal = NULL;
+    token.line = line_number;
+
+    return token;
+}
